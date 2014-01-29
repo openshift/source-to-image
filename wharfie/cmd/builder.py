ef7c6f5af3fc150558a4435a6681975494c4b85e
Krishna Raman <kraman@gmail.com>
2014-01-29 11:54:22 -0800

    Cleanup build code. Add support for timeout and environments.

--- wharfie/cmd/builder.py
+++ wharfie/cmd/builder.py
@@ -4,10 +4,8 @@ import os
 import sys
 import docopt
 import docker
-import io
 import tempfile
 import shutil
-from array import array
 
 """Wharfie is a tool for building reproducable Docker images.  Wharfie produces ready-to-run images by
 injecting a user source into a docker image and preparing a new Docker image which incorporates
@@ -19,8 +17,9 @@ class Builder(object):
     Wharfie.
 
     Usage:
-        wharfie build NEW_IMAGE_NAME IMAGE_NAME SOURCE_DIR [--incremental=OLD_IMAGE_NAME] [--user=USERID] [--url=URL]
-        wharfie validate IMAGE_NAME [--supports-incremental] [--url=URL]
+        wharfie build IMAGE_NAME SOURCE_DIR [--tag=BUILD_TAG] [--incremental=PREV_BUILD]
+            [--user=USERID] [--url=URL] [--timeout=TIMEOUT] [-e ENV_NAME=VALUE]...
+        wharfie validate IMAGE_NAME [--supports-incremental] [--url=URL] [--timeout=TIMEOUT]
         wharfie --help
 
     Arguments:
@@ -28,7 +27,9 @@ class Builder(object):
         SOURCE_DIR      Directory containing your application sources.
 
     Options:
-        --incremental=OLD_IMAGE_NAME    Perform an incremental build.
+        --incremental=PREV_BUILD        Perform an incremental build. PREV_BUILD specified the previous built image.
+        --tag=BUILD_TAG                 Image will be tagged with provided name after a successful build.
+        --timeout=TIMEOUT               Timeout commands if they take too long. Default: 120 seconds.
         --user=USERID                   Perform the build as specified user.
         --url=URL                       Connect to docker at the specified url [default: unix://var/run/docker.sock]
         --help                          Print this help message.
@@ -36,8 +37,13 @@ class Builder(object):
     def __init__(self):
         self.arguments = docopt.docopt(Builder.__doc__)
         self.docker_url = self.arguments['--url']
-        # TODO: make timeout an option or smarter
-        self.docker_client = docker.Client(base_url=self.docker_url, timeout=1000)
+
+        if self.arguments['--timeout']:
+            self.timeout = float(self.arguments['--timeout'])
+        else:
+            self.timeout = 120
+
+        self.docker_client = docker.Client(base_url=self.docker_url, timeout=self.timeout)
         server_version = self.docker_client.version()
         print "Connected to Docker server version %s. Server linux kernel: %s" % \
               (server_version['Version'],server_version['KernelVersion'])
@@ -84,7 +90,7 @@ class Builder(object):
             print("Error while creating container for image %s. %s" % (image_name, e.explanation))
             return False
 
-    def build(self, new_image_name, image_name, source_dir, incremental_image, user_id):
+    def build(self, image_name, source_dir, incremental_image, user_id, tag, envs=[]):
         tmp_dir = tempfile.mkdtemp()
 
         try:
@@ -98,50 +104,54 @@ class Builder(object):
                 exitcode = self.client.wait(container_id)
                 self.docker_client.remove_container(container)
 
-            dockerfile_lines = ["FROM %s" % image_name]
-
             build_context_source = os.path.join(tmp_dir, 'src')
             shutil.copytree(source_dir, build_context_source)
-            dockerfile_lines.append("ADD ./src /usr/src/")
 
+            docker_file = open(os.path.join(tmp_dir, 'Dockerfile'), 'w+')
+            docker_file.write("FROM %s\n" % image_name)
+            docker_file.write('ADD ./src /usr/src/\n')
             if incremental_image:
-                dockerfile_lines.append("ADD ./artifacts /usr/artifacts")
-
-            dockerfile_lines.append('RUN /usr/bin/prepare')
-            dockerfile_lines.append('CMD /usr/bin/run')
+                docker_file.write('ADD ./artifacts /usr/artifacts\n')
+            for env in envs:
+                env = env.split("=")
+                name = env[0]
+                value = env[1]
+                docker_file.write("ENV %s %s\n" % (name, value))
+            docker_file.write('RUN /usr/bin/prepare\n')
+            docker_file.write('CMD /usr/bin/run\n')
+            docker_file.close()
             
-            dockerfile = open(os.path.join(tmp_dir, 'Dockerfile'), 'w+')
-            for line in dockerfile_lines:
-                print>>dockerfile, line
-            dockerfile.close()
-
             print("Building new docker image")
-            img, logs = self.docker_client.build(tag=new_image_name, path=tmp_dir)
+            img, logs = self.docker_client.build(tag=tag, path=tmp_dir, rm=True)
             print("Build logs: %s" % logs)
-
-            print("Built image %s" % new_image_name)
+            if tag:
+                print("Built image %s" % tag)
+            else:
+                print("Built image %s" % img)
         finally:
             shutil.rmtree(tmp_dir)
 
     def main(self):
         image_name = self.arguments['IMAGE_NAME']
+        try:
+            if self.arguments['validate']:
+                self.validate_image(image_name, self.arguments['--supports-incremental']);
 
-        if self.arguments['validate']:
-            self.validate_image(image_name, self.arguments['--supports-incremental']);
+            if self.arguments['build']:
+                tag = self.arguments['--tag']
 
-        if self.arguments['build']:
-            new_image_name = self.arguments['NEW_IMAGE_NAME']
+                if self.arguments['--incremental']:
+                    if ((not self.validate_image(image_name, True)) or
+                            (not self.validate_image(self.arguments['--incremental'], True))):
+                        return -1
+                else:
+                    if not self.validate_image(image_name, False):
+                        return -1
 
-            if self.arguments['--incremental']:
-                self.validate_image(image_name, True);
-                self.validate_image(self.arguments['--incremental'], True);
-            else:
-                self.validate_image(image_name, False);
-
-            self.build(new_image_name, image_name, self.arguments['SOURCE_DIR'], self.arguments['--incremental'],
-                       self.arguments['--user'])
-
-        self.docker_client.close()
+                self.build(image_name, self.arguments['SOURCE_DIR'], self.arguments['--incremental'],
+                           self.arguments['--user'], tag, self.arguments['ENV_NAME=VALUE'])
+        finally:
+            self.docker_client.close()
 
 
 def main():
