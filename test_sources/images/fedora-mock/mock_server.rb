0f82e920983d193f0521a16f2cb0d07612a386be
Michal Fojtik <mfojtik@redhat.com>
2014-02-04 14:53:44 +0100

    Bind the application in fedora_mock to container IP address

--- test_sources/images/fedora-mock/mock_server.rb
+++ test_sources/images/fedora-mock/mock_server.rb
@@ -1,10 +1,8 @@
 #!/usr/bin/env ruby
 require 'webrick'
-require 'webrick/https'
 include WEBrick
 
 config = {}
-cert_name = [["CN",`hostname`.strip]]
 config.update(:Port => 8080)
 config.update(:BindAddress => ARGV[0])
 config.update(:DocumentRoot => ARGV[1])
