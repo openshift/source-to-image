cb8dd75e4c84c9bb033f9ff3e76d4251920f215e
csrwng <cewong@redhat.com>
2014-10-23 14:38:44 -0400

    Switch to multi-platform release build

--- hack/build-go.sh
+++ hack/build-go.sh
@@ -12,10 +12,7 @@ hackdir=$(CDPATH="" cd $(dirname $0); pwd)
 . "${hackdir}/config-go.sh"
 
 # Go to the top of the tree.
-cd "${OS_REPO_ROOT}"
-
-# Fetch the version.
-version=$(gitcommit)
+cd "${STI_REPO_ROOT}"
 
 if [[ $# == 0 ]]; then
   # Update $@ with the default list of targets to build.
@@ -24,12 +21,12 @@ fi
 
 binaries=()
 for arg; do
-  binaries+=("${OS_GO_PACKAGE}/${arg}")
+  binaries+=("${STI_GO_PACKAGE}/${arg}")
 done
 
 build_tags=""
-if [[ ! -z "$OS_BUILD_TAGS" ]]; then
-  build_tags="-tags \"$OS_BUILD_TAGS\""
+if [[ ! -z "$STI_BUILD_TAGS" ]]; then
+  build_tags="-tags \"$STI_BUILD_TAGS\""
 fi
 
-go install $build_tags -ldflags "-X github.com/openshift/source-to-image/pkg/sti/version.commitFromGit '${version}'" "${binaries[@]}"
+go install $build_tags -ldflags "$(sti::build::ldflags)" "${binaries[@]}"
