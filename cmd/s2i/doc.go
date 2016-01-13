b4b0344d8ebdbefd5ec76093a50aab17e165a232
Dan McPherson <dmcphers@redhat.com>
2016-01-13 14:19:02 -0500

    STI -> S2I

--- cmd/s2i/doc.go
+++ cmd/s2i/doc.go
@@ -1,4 +1,4 @@
-// Main STI command. Parses command line arguments, builds config object,
+// Main S2I command. Parses command line arguments, builds config object,
 // and launches the proper handler.
 
 package main
