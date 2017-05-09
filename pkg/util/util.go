cb5b034cb750c1cfdef0ed84fabc8ffb21a796d1
Ben Parees <bparees@redhat.com>
2017-05-09 18:48:01 -0400

    rework credential redacting

--- pkg/util/util.go
+++ pkg/util/util.go
@@ -1,17 +1,15 @@
 package util
 
 import (
-	"fmt"
-
 	"github.com/docker/engine-api/types/container"
 )
 
-// SafeForLoggingContainerConfig returns a string version of the container.Config object
+// SafeForLoggingContainerConfig returns a copy of the container.Config object
 // with sensitive information (proxy environment variables containing credentials)
 // redacted.
-func SafeForLoggingContainerConfig(config *container.Config) string {
-	strippedEnv := StripProxyCredentials(config.Env)
+func SafeForLoggingContainerConfig(config *container.Config) *container.Config {
+	strippedEnv := SafeForLoggingEnv(config.Env)
 	newConfig := *config
 	newConfig.Env = strippedEnv
-	return fmt.Sprintf("%+v", newConfig)
+	return &newConfig
 }
