8c296b03544c0e19023702fb2d10a56459d751f3
Adam Kaplan <adam.kaplan@redhat.com>
2018-05-31 11:15:44 -0400

    Revert "Merge pull request #818 from soltysh/bump_docker"
    This docker bump was not included in the origin 3.7.0 release.
    
    This reverts commit e3140d019517368c7c3f72476f9cae7a8b1269d0, reversing
    changes made to 9a1bd4dcd19bca938c3f8aa09cf1748a1560554d.

--- vendor/github.com/docker/docker/builder/dockerfile/parser/line_parsers.go
+++ vendor/github.com/docker/docker/builder/dockerfile/parser/line_parsers.go
@@ -10,25 +10,17 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"sort"
 	"strings"
 	"unicode"
-	"unicode/utf8"
-
-	"github.com/docker/docker/builder/dockerfile/command"
 )
 
 var (
 	errDockerfileNotStringArray = errors.New("When using JSON array syntax, arrays must be comprised of strings only.")
 )
 
-const (
-	commandLabel = "LABEL"
-)
-
 // ignore the current argument. This will still leave a command parsed, but
 // will not incorporate the arguments into the ast.
-func parseIgnore(rest string, d *Directive) (*Node, map[string]bool, error) {
+func parseIgnore(rest string) (*Node, map[string]bool, error) {
 	return &Node{}, nil, nil
 }
 
@@ -37,12 +29,12 @@ func parseIgnore(rest string, d *Directive) (*Node, map[string]bool, error) {
 //
 // ONBUILD RUN foo bar -> (onbuild (run foo bar))
 //
-func parseSubCommand(rest string, d *Directive) (*Node, map[string]bool, error) {
+func parseSubCommand(rest string) (*Node, map[string]bool, error) {
 	if rest == "" {
 		return nil, nil, nil
 	}
 
-	child, err := newNodeFromLine(rest, d)
+	_, child, err := ParseLine(rest)
 	if err != nil {
 		return nil, nil, err
 	}
@@ -53,7 +45,7 @@ func parseSubCommand(rest string, d *Directive) (*Node, map[string]bool, error) 
 // helper to parse words (i.e space delimited or quoted strings) in a statement.
 // The quotes are preserved as part of this function and they are stripped later
 // as part of processWords().
-func parseWords(rest string, d *Directive) []string {
+func parseWords(rest string) []string {
 	const (
 		inSpaces = iota // looking for start of a word
 		inWord
@@ -66,11 +58,10 @@ func parseWords(rest string, d *Directive) []string {
 	quote := '\000'
 	blankOK := false
 	var ch rune
-	var chWidth int
 
-	for pos := 0; pos <= len(rest); pos += chWidth {
+	for pos := 0; pos <= len(rest); pos++ {
 		if pos != len(rest) {
-			ch, chWidth = utf8.DecodeRuneInString(rest[pos:])
+			ch = rune(rest[pos])
 		}
 
 		if phase == inSpaces { // Looking for start of word
@@ -103,16 +94,16 @@ func parseWords(rest string, d *Directive) []string {
 				blankOK = true
 				phase = inQuote
 			}
-			if ch == d.escapeToken {
-				if pos+chWidth == len(rest) {
-					continue // just skip an escape token at end of line
+			if ch == '\\' {
+				if pos+1 == len(rest) {
+					continue // just skip \ at end
 				}
-				// If we're not quoted and we see an escape token, then always just
-				// add the escape token plus the char to the word, even if the char
+				// If we're not quoted and we see a \, then always just
+				// add \ plus the char to the word, even if the char
 				// is a quote.
 				word += string(ch)
-				pos += chWidth
-				ch, chWidth = utf8.DecodeRuneInString(rest[pos:])
+				pos++
+				ch = rune(rest[pos])
 			}
 			word += string(ch)
 			continue
@@ -121,15 +112,16 @@ func parseWords(rest string, d *Directive) []string {
 			if ch == quote {
 				phase = inWord
 			}
-			// The escape token is special except for ' quotes - can't escape anything for '
-			if ch == d.escapeToken && quote != '\'' {
-				if pos+chWidth == len(rest) {
+			// \ is special except for ' quotes - can't escape anything for '
+			if ch == '\\' && quote != '\'' {
+				if pos+1 == len(rest) {
 					phase = inWord
-					continue // just skip the escape token at end
+					continue // just skip \ at end
 				}
-				pos += chWidth
+				pos++
+				nextCh := rune(rest[pos])
 				word += string(ch)
-				ch, chWidth = utf8.DecodeRuneInString(rest[pos:])
+				ch = nextCh
 			}
 			word += string(ch)
 		}
@@ -140,97 +132,66 @@ func parseWords(rest string, d *Directive) []string {
 
 // parse environment like statements. Note that this does *not* handle
 // variable interpolation, which will be handled in the evaluator.
-func parseNameVal(rest string, key string, d *Directive) (*Node, error) {
+func parseNameVal(rest string, key string) (*Node, map[string]bool, error) {
 	// This is kind of tricky because we need to support the old
 	// variant:   KEY name value
 	// as well as the new one:    KEY name=value ...
 	// The trigger to know which one is being used will be whether we hit
 	// a space or = first.  space ==> old, "=" ==> new
 
-	words := parseWords(rest, d)
+	words := parseWords(rest)
 	if len(words) == 0 {
-		return nil, nil
+		return nil, nil, nil
 	}
+
+	var rootnode *Node
 
 	// Old format (KEY name value)
 	if !strings.Contains(words[0], "=") {
-		parts := tokenWhitespace.Split(rest, 2)
-		if len(parts) < 2 {
-			return nil, fmt.Errorf(key + " must have two arguments")
-		}
-		return newKeyValueNode(parts[0], parts[1]), nil
-	}
+		node := &Node{}
+		rootnode = node
+		strs := tokenWhitespace.Split(rest, 2)
 
-	var rootNode *Node
-	var prevNode *Node
-	for _, word := range words {
-		if !strings.Contains(word, "=") {
-			return nil, fmt.Errorf("Syntax error - can't find = in %q. Must be of the form: name=value", word)
+		if len(strs) < 2 {
+			return nil, nil, fmt.Errorf(key + " must have two arguments")
 		}
 
-		parts := strings.SplitN(word, "=", 2)
-		node := newKeyValueNode(parts[0], parts[1])
-		rootNode, prevNode = appendKeyValueNode(node, rootNode, prevNode)
+		node.Value = strs[0]
+		node.Next = &Node{}
+		node.Next.Value = strs[1]
+	} else {
+		var prevNode *Node
+		for i, word := range words {
+			if !strings.Contains(word, "=") {
+				return nil, nil, fmt.Errorf("Syntax error - can't find = in %q. Must be of the form: name=value", word)
+			}
+			parts := strings.SplitN(word, "=", 2)
+
+			name := &Node{}
+			value := &Node{}
+
+			name.Next = value
+			name.Value = parts[0]
+			value.Value = parts[1]
+
+			if i == 0 {
+				rootnode = name
+			} else {
+				prevNode.Next = name
+			}
+			prevNode = value
+		}
 	}
 
-	return rootNode, nil
+	return rootnode, nil, nil
 }
 
-func newKeyValueNode(key, value string) *Node {
-	return &Node{
-		Value: key,
-		Next:  &Node{Value: value},
-	}
+func parseEnv(rest string) (*Node, map[string]bool, error) {
+	return parseNameVal(rest, "ENV")
 }
 
-func appendKeyValueNode(node, rootNode, prevNode *Node) (*Node, *Node) {
-	if rootNode == nil {
-		rootNode = node
-	}
-	if prevNode != nil {
-		prevNode.Next = node
-	}
-
-	prevNode = node.Next
-	return rootNode, prevNode
-}
-
-func parseEnv(rest string, d *Directive) (*Node, map[string]bool, error) {
-	node, err := parseNameVal(rest, "ENV", d)
-	return node, nil, err
-}
-
-func parseLabel(rest string, d *Directive) (*Node, map[string]bool, error) {
-	node, err := parseNameVal(rest, commandLabel, d)
-	return node, nil, err
-}
-
-// NodeFromLabels returns a Node for the injected labels
-func NodeFromLabels(labels map[string]string) *Node {
-	keys := []string{}
-	for key := range labels {
-		keys = append(keys, key)
-	}
-	// Sort the label to have a repeatable order
-	sort.Strings(keys)
-
-	labelPairs := []string{}
-	var rootNode *Node
-	var prevNode *Node
-	for _, key := range keys {
-		value := labels[key]
-		labelPairs = append(labelPairs, fmt.Sprintf("%q='%s'", key, value))
-		// Value must be single quoted to prevent env variable expansion
-		// See https://github.com/docker/docker/issues/26027
-		node := newKeyValueNode(key, "'"+value+"'")
-		rootNode, prevNode = appendKeyValueNode(node, rootNode, prevNode)
-	}
-
-	return &Node{
-		Value:    command.Label,
-		Original: commandLabel + " " + strings.Join(labelPairs, " "),
-		Next:     rootNode,
-	}
+func parseLabel(rest string) (*Node, map[string]bool, error) {
+	return parseNameVal(rest, "LABEL")
 }
 
 // parses a statement containing one or more keyword definition(s) and/or
@@ -241,8 +202,8 @@ func NodeFromLabels(labels map[string]string) *Node {
 // In addition, a keyword definition alone is of the form `keyword` like `name1`
 // above. And the assignments `name2=` and `name3=""` are equivalent and
 // assign an empty value to the respective keywords.
-func parseNameOrNameVal(rest string, d *Directive) (*Node, map[string]bool, error) {
-	words := parseWords(rest, d)
+func parseNameOrNameVal(rest string) (*Node, map[string]bool, error) {
+	words := parseWords(rest)
 	if len(words) == 0 {
 		return nil, nil, nil
 	}
@@ -267,7 +228,7 @@ func parseNameOrNameVal(rest string, d *Directive) (*Node, map[string]bool, erro
 
 // parses a whitespace-delimited set of arguments. The result is effectively a
 // linked list of string arguments.
-func parseStringsWhitespaceDelimited(rest string, d *Directive) (*Node, map[string]bool, error) {
+func parseStringsWhitespaceDelimited(rest string) (*Node, map[string]bool, error) {
 	if rest == "" {
 		return nil, nil, nil
 	}
@@ -290,8 +251,8 @@ func parseStringsWhitespaceDelimited(rest string, d *Directive) (*Node, map[stri
 	return rootnode, nil, nil
 }
 
-// parseString just wraps the string in quotes and returns a working node.
-func parseString(rest string, d *Directive) (*Node, map[string]bool, error) {
+// parsestring just wraps the string in quotes and returns a working node.
+func parseString(rest string) (*Node, map[string]bool, error) {
 	if rest == "" {
 		return nil, nil, nil
 	}
@@ -301,7 +262,7 @@ func parseString(rest string, d *Directive) (*Node, map[string]bool, error) {
 }
 
 // parseJSON converts JSON arrays to an AST.
-func parseJSON(rest string, d *Directive) (*Node, map[string]bool, error) {
+func parseJSON(rest string) (*Node, map[string]bool, error) {
 	rest = strings.TrimLeftFunc(rest, unicode.IsSpace)
 	if !strings.HasPrefix(rest, "[") {
 		return nil, nil, fmt.Errorf(`Error parsing "%s" as a JSON array`, rest)
@@ -334,12 +295,12 @@ func parseJSON(rest string, d *Directive) (*Node, map[string]bool, error) {
 // parseMaybeJSON determines if the argument appears to be a JSON array. If
 // so, passes to parseJSON; if not, quotes the result and returns a single
 // node.
-func parseMaybeJSON(rest string, d *Directive) (*Node, map[string]bool, error) {
+func parseMaybeJSON(rest string) (*Node, map[string]bool, error) {
 	if rest == "" {
 		return nil, nil, nil
 	}
 
-	node, attrs, err := parseJSON(rest, d)
+	node, attrs, err := parseJSON(rest)
 
 	if err == nil {
 		return node, attrs, nil
@@ -356,8 +317,8 @@ func parseMaybeJSON(rest string, d *Directive) (*Node, map[string]bool, error) {
 // parseMaybeJSONToList determines if the argument appears to be a JSON array. If
 // so, passes to parseJSON; if not, attempts to parse it as a whitespace
 // delimited string.
-func parseMaybeJSONToList(rest string, d *Directive) (*Node, map[string]bool, error) {
-	node, attrs, err := parseJSON(rest, d)
+func parseMaybeJSONToList(rest string) (*Node, map[string]bool, error) {
+	node, attrs, err := parseJSON(rest)
 
 	if err == nil {
 		return node, attrs, nil
@@ -366,34 +327,5 @@ func parseMaybeJSONToList(rest string, d *Directive) (*Node, map[string]bool, er
 		return nil, nil, err
 	}
 
-	return parseStringsWhitespaceDelimited(rest, d)
-}
-
-// The HEALTHCHECK command is like parseMaybeJSON, but has an extra type argument.
-func parseHealthConfig(rest string, d *Directive) (*Node, map[string]bool, error) {
-	// Find end of first argument
-	var sep int
-	for ; sep < len(rest); sep++ {
-		if unicode.IsSpace(rune(rest[sep])) {
-			break
-		}
-	}
-	next := sep
-	for ; next < len(rest); next++ {
-		if !unicode.IsSpace(rune(rest[next])) {
-			break
-		}
-	}
-
-	if sep == 0 {
-		return nil, nil, nil
-	}
-
-	typ := rest[:sep]
-	cmd, attrs, err := parseMaybeJSON(rest[next:], d)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	return &Node{Value: typ, Next: cmd}, attrs, err
+	return parseStringsWhitespaceDelimited(rest)
 }
--- vendor/github.com/docker/docker/pkg/term/term.go
+++ vendor/github.com/docker/docker/pkg/term/term.go
@@ -1,17 +1,16 @@
 // +build !windows
 
-// Package term provides structures and helper functions to work with
+// Package term provides provides structures and helper functions to work with
 // terminal (state, sizes).
 package term
 
 import (
 	"errors"
-	"fmt"
 	"io"
 	"os"
 	"os/signal"
-
-	"golang.org/x/sys/unix"
+	"syscall"
+	"unsafe"
 )
 
 var (
@@ -32,7 +31,7 @@ type Winsize struct {
 	y      uint16
 }
 
-// StdStreams returns the standard streams (stdin, stdout, stderr).
+// StdStreams returns the standard streams (stdin, stdout, stedrr).
 func StdStreams() (stdIn io.ReadCloser, stdOut, stdErr io.Writer) {
 	return os.Stdin, os.Stdout, os.Stderr
 }
@@ -46,6 +45,27 @@ func GetFdInfo(in interface{}) (uintptr, bool) {
 		isTerminalIn = IsTerminal(inFd)
 	}
 	return inFd, isTerminalIn
+}
+
+// GetWinsize returns the window size based on the specified file descriptor.
+func GetWinsize(fd uintptr) (*Winsize, error) {
+	ws := &Winsize{}
+	_, _, err := syscall.Syscall(syscall.SYS_IOCTL, fd, uintptr(syscall.TIOCGWINSZ), uintptr(unsafe.Pointer(ws)))
+	// Skip errno = 0
+	if err == 0 {
+		return ws, nil
+	}
+	return ws, err
+}
+
+// SetWinsize tries to set the specified window size for the specified file descriptor.
+func SetWinsize(fd uintptr, ws *Winsize) error {
+	_, _, err := syscall.Syscall(syscall.SYS_IOCTL, fd, uintptr(syscall.TIOCSWINSZ), uintptr(unsafe.Pointer(ws)))
+	// Skip errno = 0
+	if err == 0 {
+		return nil
+	}
+	return err
 }
 
 // IsTerminal returns true if the given file descriptor is a terminal.
@@ -80,7 +100,7 @@ func SaveState(fd uintptr) (*State, error) {
 // descriptor, with echo disabled.
 func DisableEcho(fd uintptr, state *State) error {
 	newState := state.termios
-	newState.Lflag &^= unix.ECHO
+	newState.Lflag &^= syscall.ECHO
 
 	if err := tcset(fd, &newState); err != 0 {
 		return err
@@ -90,8 +110,7 @@ func DisableEcho(fd uintptr, state *State) error {
 }
 
 // SetRawTerminal puts the terminal connected to the given file descriptor into
-// raw mode and returns the previous state. On UNIX, this puts both the input
-// and output into raw mode. On Windows, it only puts the input into raw mode.
+// raw mode and returns the previous state.
 func SetRawTerminal(fd uintptr) (*State, error) {
 	oldState, err := MakeRaw(fd)
 	if err != nil {
@@ -101,24 +120,12 @@ func SetRawTerminal(fd uintptr) (*State, error) {
 	return oldState, err
 }
 
-// SetRawTerminalOutput puts the output of terminal connected to the given file
-// descriptor into raw mode. On UNIX, this does nothing and returns nil for the
-// state. On Windows, it disables LF -> CRLF translation.
-func SetRawTerminalOutput(fd uintptr) (*State, error) {
-	return nil, nil
-}
-
 func handleInterrupt(fd uintptr, state *State) {
 	sigchan := make(chan os.Signal, 1)
 	signal.Notify(sigchan, os.Interrupt)
+
 	go func() {
-		for range sigchan {
-			// quit cleanly and the new terminal item is on a new line
-			fmt.Println()
-			signal.Stop(sigchan)
-			close(sigchan)
-			RestoreTerminal(fd, state)
-			os.Exit(1)
-		}
+		_ = <-sigchan
+		RestoreTerminal(fd, state)
 	}()
 }
