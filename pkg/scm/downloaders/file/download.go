d56b9e8b179047e3f7aefeb29aa3b7841956ffb5
Ben Parees <bparees@redhat.com>
2018-07-17 16:33:13 -0400

    disallow copies to target into the source dir of the copy

--- pkg/scm/downloaders/file/download.go
+++ pkg/scm/downloaders/file/download.go
@@ -1,7 +1,9 @@
 package file
 
 import (
+	"fmt"
 	"path/filepath"
+	"strings"
 
 	"github.com/openshift/source-to-image/pkg/api"
 	"github.com/openshift/source-to-image/pkg/api/constants"
@@ -11,6 +13,10 @@ import (
 )
 
 var glog = utilglog.StderrLog
+
+type RecursiveCopyError struct {
+	error
+}
 
 // File represents a simplest possible Downloader implementation where the
 // sources are just copied from local directory.
@@ -29,6 +35,9 @@ func (f *File) Download(config *api.Config) (*git.SourceInfo, error) {
 	}
 
 	glog.V(1).Infof("Copying sources from %q to %q", copySrc, config.WorkingSourceDir)
+	if strings.HasPrefix(filepath.Clean(config.WorkingSourceDir), filepath.Clean(copySrc)) {
+		return nil, RecursiveCopyError{error: fmt.Errorf("recursive copy requested, source directory %q contains the target directory %q", copySrc, config.WorkingSourceDir)}
+	}
 	if copySrc != config.WorkingSourceDir {
 		err := f.CopyContents(copySrc, config.WorkingSourceDir)
 		if err != nil {
