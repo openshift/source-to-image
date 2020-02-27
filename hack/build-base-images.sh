b44eca0c79e533ac3e173f9bf32954c9d0993248
Igor Sutton Lopes <isuttonl@redhat.com>
2020-02-27 11:04:35 +0100

    Use S2I_GIT_COMMIT as image label
    Both `make image` and `make test-image` use the sti-version-defs
    file to understand the image it should be testing for the current
    working directory.

--- hack/build-base-images.sh
+++ hack/build-base-images.sh
@@ -9,11 +9,16 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-S2I_ROOT=$(dirname "${BASH_SOURCE}")/..
+S2I_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
 source "${S2I_ROOT}/hack/common.sh"
 
+s2i::build::get_version_vars
+s2i::build::save_version_vars "${S2I_ROOT}/sti-version-defs"
+
 # Go to the top of the tree.
-cd "${S2I_ROOT}"
+pushd "${S2I_ROOT}"
 
 # Build the images
-docker build --tag openshift/sti-release "${S2I_ROOT}/images/release"
+docker build --tag "openshift/sti-release:${S2I_GIT_COMMIT}" "${S2I_ROOT}" -f "${S2I_ROOT}/images/release/Dockerfile"
+
+popd
